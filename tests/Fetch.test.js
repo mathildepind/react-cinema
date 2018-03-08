6ff1540af6949424f6e97f6146801355f060a1c3
mathildepind <mathilde.pind.rasmussen@gmail.com>
2018-03-08 18:15:58 +0000

    update test

--- tests/Fetch.test.js
+++ tests/Fetch.test.js
@@ -15,15 +15,15 @@ describe('App fetch', () => {
 
     it('it starts with a movie', done => {
       const wrapper = shallow(<App />);
-      const button = wrapper.find('button').simulate('click');
-      // wrapper.instance().handleClickReceiver()
+      // const button = wrapper.find('button').simulate('click');
+      wrapper.instance().handleClickReceiver()
 
       setTimeout(() => {
         const updated = wrapper.update();
-        const text = updated.find('h1').text();
-        expect(text).toEqual('The Best Test Movie');
-        // const renderedResultComponent = updated.find(<Result movie={movie}/>);
-        // expect(renderedResultComponent).toEqual('')
+        // const text = updated.find('h1').text();
+        // expect(text).toEqual('The Best Test Movie');
+        const renderedResultComponent = updated.find(<Result movie={movie}/>);
+        expect(renderedResultComponent).toEqual('')
         done();
       }, 0);
     });
